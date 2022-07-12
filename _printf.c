0f2d218d74bb0d775a62dc2a44d9f9cffc2871d7
Enos22 <Enos22>
2022-07-12 09:27:46 -0700

    Diana and Enos Printf project

--- _printf.c
+++ _printf.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _printf: funtion for the printf format
+ * _print - funtion for the printf format
  * @format: lists the arguments passed
  * Description: select the format
  * Return: Always 0
@@ -16,43 +16,33 @@ int _print(cont char *format)
 		{"%", _printp}
 		{"d", -printd}
 		{"i", _printi}
-		{NULL, NULL} 
+		{NULL, NULL}
 	};
 	va_start(valist, format);
 	if (!format || (format[0] == '%' && format[1] == '\0'))
-			return (-1)
-				for (i =0; format != NULL && format [i]= != '\0', 1++)
-				{
-					if (format [i] == '%')
-					{
-						if (format [i + 1] == '%')
-						{
-							_putchar('%')
-								j++
-								i++
-						}
-						else if (typefor(format, i +1) != NULL
-								{
-								j +=  _typefor (format, i +1 (valist));
-								i ++;
-								}
-								}
-								else
-								{
-								_putchar (format [i]);
-								j++
-								}
-								}
-								va_end (valist)
-								return (j);
+		return (-1);
+	for (i = 0; format != NULL && format[i] = != '\0', 1++)
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '%')
+			{
+				_putchar('%')
+					j++;
+				i++;
+			}
+			else if (typefor(format, i + 1) != NULL)
+			{
+				j +=  _typefor(format, i + 1 (valist));
+				i++;
+			}
+			else
+			{
+				_putchar(format[i]);
+				j++;
+			}
+		}
+		va_end(valist)
+			return (j);
+	}
 }
-
-
-								
-
-								
-								
-								
-
-
-
